5984849130ad634c00d37b52db40e555caf1a36c
Domino <thathayho91@gmail.com>
2025-04-03 14:12:18 +0700

    [add] Do an mon hoc LTW

---
+++ Nhom5_WebThuongMaiDienTu/Program.cs
@@ -0,0 +1,28 @@
+using Nhom5_WebThuongMaiDienTu.Models.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<OnlineShopContext>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapControllerRoute(
+        name: "Admin",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
